39dc04a9f0cdc01ae565e1bb82a27905e5810eae
unknown <vatsal156thaker@gmail.com>
2024-04-03 18:41:45 +0530

    added function to helper

--- helpers.php
+++ helpers.php
@@ -27,6 +27,7 @@ function basePath($path = '')
 function loadView($name)
 {
     $viewPath =  basePath("views/{$name}.view.php");
+    inspect($viewPath);
 
     if (file_exists($viewPath)) {
         require $viewPath;
@@ -53,4 +54,38 @@ function loadPartial($name)
     } else {
         echo "View '{$name} not Found! '";
     }
+}
+
+
+/**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ * 
+ * 
+ * 
+ */
+function inspect($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+}
+
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ * 
+ * 
+ * 
+ */
+function inspectAndDie($value)
+{
+    echo "<pre>";
+    die(var_dump($value));
+    echo "</pre>";
 }
